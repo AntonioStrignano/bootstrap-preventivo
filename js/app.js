ff3dd5cd080ac0a091037e5ad8836f6e314b137b
Antonio-Strignano <a.strignano@yahoo.it>
2024-04-04 19:14:57 +0200

    ADD: calcolo prezzo e visualizzazione prezzo calcolato

--- js/app.js
+++ js/app.js
@@ -26,20 +26,22 @@ const jobs = [
     { jobLabel: 'Project Analysis', rate: 33.6, selectValue: 'analysis' },
 ];
 
+// console.log(jobs);
+
 const jobList = []
 
 jobs.forEach(function (job) {
     jobList.push('<option value="' + job.selectValue + '">' + job.jobLabel + '</option>');
 })
 
-let selectJob = ''
-console.log(selectJob);
+let selectJobs = ''
+console.log(selectJobs);
 
 jobList.forEach(function (option) {
-    selectJob = selectJob + option
+    selectJobs = selectJobs + option
 })
 
-document.getElementById("jobSelect").innerHTML = selectJob
+document.getElementById("jobSelect").innerHTML = selectJobs
 
 
 // FUNZIONE CALCOLO PREZZO
@@ -47,7 +49,55 @@ document.getElementById("jobSelect").innerHTML = selectJob
 // se inserito codice sconto allora parte ciclo che verifica se il codice sconto inserito esiste nell'array di codici sconto funzionanti e prezzo pieno * 3 / 4 = prezzo finale
 // POI FORMATTA prezzo finale con due cifre decimali, dividi in in array numero intero e parte decimale
 
+const promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
+let fullPrice = 0
+
+function fullPriceCalc() {
+    const selectedValue = document.querySelector("#jobSelect").value;
+    const selectedRate = jobs.find((element) => element.selectValue == selectedValue).rate
+    fullPrice = selectedRate * 10;
+}
+
+let finalPrice = 0
+
+function promo() {
+    if (
+        promoCodes.includes(document.getElementById("codeForm").value) == true
+    ) {
+        finalPrice = (fullPrice * 3) / 4;
+    }
+    else {
+        finalPrice = fullPrice
+    }
+}
+
+let splittedPrice = []
+
+function writePrice() {
+    finalPrice = finalPrice.toFixed(2)
+    splittedPrice = finalPrice.split('.')
+    document.getElementById("price").innerHTML = `<strong class="fs-2 text-black">&euro; ${splittedPrice[0]}</strong>,${splittedPrice[1]}`
+}
+
+function debug() {
+
+    console.log('fullPrice 2 = ' + fullPrice);
+    console.log(`il codice esiste? ${promoCodes.includes(document.getElementById("codeForm").value)}`)
+    console.log(`final price = ${finalPrice}`);
+    console.log(splittedPrice);
+}
+
+function submitClick() {
+
+}
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener('click', submitClick)
+submitButton.addEventListener('click', fullPriceCalc)
+submitButton.addEventListener('click', promo)
+submitButton.addEventListener('click', writePrice)
+// submitButton.addEventListener('click', debug)
 
 // QUINDI
 // listener del submit (il form è in method dialog, quindi non invia), se NON tutti i campi sono pieni stampa "compilare tutti i campi", altrimenti invoca funzione del calcolo prezzo
